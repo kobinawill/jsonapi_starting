675a726f23613654aaf9c960aab554ce326679d8
kobinawill <kobinalwill@gmail.com>
2020-04-03 10:20:15 +0000

    parsing jason information into app and applying the ListView.builder widget

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  List _data = await getUi();
+  runApp(new MaterialApp(
+    title: "JSON Parse" ,
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("JSON Parse"),
+        backgroundColor: Colors.orangeAccent,
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: new ListView.builder(
+            itemCount: _data.length,
+            itemBuilder: (BuildContext context, int indexValue){
+              return new Column(
+                children: <Widget>[
+                  new Divider(height: 5.5,),
+                  new ListTile(
+                    title: new Text(_data[indexValue]['title']),
+                    subtitle: new Text(_data[indexValue]['body']),
+                    leading: new CircleAvatar(
+                      child: new Text("${_data[indexValue]['id']}"),
+                      backgroundColor: Colors.brown.shade900,
+                    ),
+                  )
+                ],
+              );
+            }),
+      ),
+    ),
+  ));
+}
+
+Future<List> getUi() async {
+    http.Response theResponse = await http.get("https://jsonplaceholder.typicode.com/posts");
+    return json.decode(theResponse.body);
+}
